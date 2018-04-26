94a7de8c2c84dce8467b29c99785a455c7913112
Nikita Sharma <nikita.s2692@gmail.com>
2018-04-26 23:35:26 +0530

    Integrated material-ui@next and added a sample code. (#22)
    #14 material-ui@next and saas setup with sample code.

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,68 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import Button from 'material-ui/Button';
+import Dialog, {
+    DialogTitle,
+    DialogContent,
+    DialogContentText,
+    DialogActions,
+} from 'material-ui/Dialog';
+import Typography from 'material-ui/Typography';
+import { withStyles } from 'material-ui/styles';
+import withRoot from "./withRoot";
 import './App.css';
+const styles = theme => ({
+    root: {
+        textAlign: 'center',
+        paddingTop: theme.spacing.unit * 20,
+    },
+});
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+class App extends React.Component {
+    state = {
+        open: false,
+    };
+
+    handleClose = () => {
+        this.setState({
+            open: false,
+        });
+    };
+
+    handleClick = () => {
+        this.setState({
+            open: true,
+        });
+    };
+
+    render() {
+        const { classes } = this.props;
+        const { open } = this.state;
+
+        return (
+            <div className={classes.root}>
+                <Dialog open={open} onClose={this.handleClose}>
+                    <DialogTitle>Test Popup</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>Hello World</DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button color="primary" onClick={this.handleClose}>
+                            OK
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+                <Typography variant="display1" gutterBottom>
+                    Material-UI
+                </Typography>
+                <Typography variant="subheading" gutterBottom>
+                    Showcase App
+                </Typography>
+                <Button variant="raised" color="secondary" onClick={this.handleClick}>
+                    Test Popup
+                </Button>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default withRoot(withStyles(styles)(App));

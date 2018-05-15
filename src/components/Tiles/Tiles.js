2b422dbb5fdc53783b19fa6dd1ac247296d32d91
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2018-05-15 06:20:08 +0530

    Fixes some styles of the Tile component (#63)
    * Fixed the shadow, background, spacing
    
    * Improved Color conversion

--- src/components/Tiles/Tiles.js
+++ src/components/Tiles/Tiles.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import './Tiles.css';
 
+const hexToRgbA = (hex) => {
+  /**
+   * Converts hex color to rgba type that will used in box-shadow
+   */
+  let rgb = [
+    parseInt(hex.slice(1, 3), 16),
+    parseInt(hex.slice(3, 5), 16),
+    parseInt(hex.slice(5, 7), 16)
+  ];
+
+  return `rgba(${rgb.join()}, 0.5)`;
+}
+
+const darkenColor = (color) => {
+  const rgb = [color.substring(1,3), color.substring(3,5), color.substring(5,7)];
+  return `rgb(${rgb.map(c => Math.round((parseInt(c, 16) * 0.55))).join()})`;
+}
+
 const Tile = (props) => {
-  const { backgroundColor } = props;
+  const { color } = props;
   const tileStyles = {
-    backgroundColor,
+    backgroundColor: color,
+    backgroundImage: `linear-gradient(135deg, ${color} 1%, ${darkenColor(color)} 100%)`,
+    boxShadow: `0px 5px 30px ${hexToRgbA(color)}`
   }
   return(
     <div className="tile-style" style={tileStyles}>
